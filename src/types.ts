edeeebe61a2b5641dacf9ee94c43c1cc40e3ea1d
Julien Simon <julien@julien.org>
2026-04-23 15:34:52 +0200

    feat: define public DishStatus, DishHistory, SpeedTestResult, MockOptions types

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,41 @@
-// types defined in Task 2
-export {};
+export interface DishStatus {
+  deviceId: string;
+  hardwareVersion: string;
+  softwareVersion: string;
+  countryCode: string;
+  bootcount: number;
+  uptimeSeconds: number;
+  state: 'CONNECTED' | 'UNKNOWN';
+  downlinkThroughputBps: number;
+  uplinkThroughputBps: number;
+  popPingLatencyMs: number;
+  popPingDropRate: number;
+  obstructionPercentTime: number;
+  currentlyObstructed: boolean;
+  snrAboveNoiseFloor: boolean;
+  snrPersistentlyLow: boolean;
+  boresightAzimuthDeg: number;
+  boresightElevationDeg: number;
+  gpsValid: boolean;
+  gpsSats: number;
+  ethSpeedMbps: number;
+  alerts: string[];
+}
+
+export interface DishHistory {
+  current: number;
+  pingLatencyMs: number[];
+  pingDropRate: number[];
+  downlinkThroughputBps: number[];
+  uplinkThroughputBps: number[];
+}
+
+export interface SpeedTestResult {
+  downloadMbps: number;
+  uploadMbps: number;
+  latencyMs: number;
+}
+
+export interface MockOptions {
+  faultRate?: number;
+}
